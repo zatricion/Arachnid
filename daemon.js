b80f5c99d59bea3f1abb38007b4a605a337c20ac
Ian Whitlock <ian@theknown.net>
2013-07-11 16:25:44 -0400

    storage, more ideas, some testing

--- daemon.js
+++ daemon.js
@@ -1,2 +1,10 @@
+function saveGraph() {
+	chrome.local.set({
+		'node':details
+	}, function(){
+		console.log(details.url);
+	});
+}
 
-chrome.webNavigation.onCommitted.addListener(function(details) { console.log(details.url); });
+chrome.webNavigation.onCommitted.addListener(function(details) { saveGraph() });
+
