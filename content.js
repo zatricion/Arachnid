1dc8d48989f7af940517b05f2247d3498556ebcb
Ian Whitlock <ian@theknown.net>
2013-07-15 18:10:18 -0400

    end of monday commit just for easy distribution. set() still not working.

--- content.js
+++ content.js
@@ -1,8 +1,8 @@
+;
+
 var runtimeOrExtension = chrome.runtime && chrome.runtime.sendMessage ?
                          'runtime' : 'extension';
 
-chrome[runtimeOrExtension].sendMessage({referrer:document.referrer, url:document.URL}, function(response) {  
-  console.log("message passed: " + document.referrer);
-});
+chrome[runtimeOrExtension].sendMessage({referrer:document.referrer, url:document.URL}, function() {});
 
 
--- daemon.js
+++ daemon.js
@@ -1,11 +1,4 @@
-function saveGraph() {
-  chrome.local.set({
-    'node':details
-  }, function(){
-    console.log(details.url);
-  });
-}
-
+;
 
 function addNode(url, referrer) {
   nodes = chrome.storage.local;
@@ -14,15 +7,34 @@ function addNode(url, referrer) {
     timestamp: Date()
   };
 
-  current_node = nodes.get(url, function(){console.log("got shit")});
-  if ( current_node ) {
-    current_node.append(edge);
-    nodes.remove(url);
-    nodes.set({url:current_node}, console.log("Updated Node with url " + url + " and created new edge from " + edge.in_node + " at time " + edge.timestamp));
-  } else {
-    nodes.set({url:[edge]}, console.log("Created new Node for url " + url + " and new edge from " + edge.in_node + " at time " + edge.timestamp));
-  }
-
+  nodes.get(url, function(current_node){
+    console.log(current_node);
+    if ( $.isEmptyObject(current_node) === false ) {
+      current_node[url].push(edge);
+      nodes.remove(url);
+      nodes.set({url:current_node}, function(response){
+        if ( chrome.runtime.lastError ) {
+          console.log(chrome.runtime.lastError);
+        }
+        else {
+          console.log("Updated Node with url " + url + " and created new edge from " + edge.in_node + " at time " + edge.timestamp);
+        }
+      });
+    }
+    else {
+      console.log("set: "+url);
+      nodes.set({url:[edge]}, function(){
+        if ( chrome.runtime.lastError ) {
+          console.log(chrome.runtime.lastError);
+        }
+        else {
+          console.log("get: "+url);
+          chrome.storage.local.get(url, function(thing) {console.log(thing)});
+          console.log("Created new Node for url " + url + " and new edge from " + edge.in_node + " at time " + edge.timestamp);
+        }
+      });
+    }
+  });
 }
 
 var runtimeOrExtension = chrome.runtime && chrome.runtime.sendMessage ?
@@ -30,6 +42,5 @@ var runtimeOrExtension = chrome.runtime && chrome.runtime.sendMessage ?
 
 chrome[runtimeOrExtension].onMessage.addListener(
   function(request, sender, sendResponse) {
-    console.log("url: "+ request.url + " and referrer: " + request.referrer);
     addNode(request.url, request.referrer);
  });
