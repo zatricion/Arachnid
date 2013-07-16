3c4991bff7f467a1efcc082c7e30682af2e48d15
zatricion <zatricion@gmail.com>
2013-07-16 13:02:25 -0400

    strip http/https from urls

--- content.js
+++ content.js
@@ -1,8 +1,11 @@
 ;
 
+URL = document.URL;
+URL = URL.substr(strig.indexOf(':') + 1);
+
 var runtimeOrExtension = chrome.runtime && chrome.runtime.sendMessage ?
                          'runtime' : 'extension';
 
-chrome[runtimeOrExtension].sendMessage({referrer:document.referrer, url:document.URL}, function() {});
+chrome[runtimeOrExtension].sendMessage({referrer:document.referrer, url:URL}, function() {});
 
 
