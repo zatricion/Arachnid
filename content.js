57bcc0df672fd47235fb8a23225495927fa15304
zatricion <zatricion@gmail.com>
2013-07-16 13:40:02 -0400

    fixed stupid bug in http stripping and now strip referrer as well

--- content.js
+++ content.js
@@ -1,11 +1,14 @@
 ;
 
 URL = document.URL;
-URL = URL.substr(strig.indexOf(':') + 1);
+URL = URL.substr(URL.indexOf(':') + 1);
+
+REF = document.referrer;
+REF = REF.substr(REF.indexOf(':') + 1);
 
 var runtimeOrExtension = chrome.runtime && chrome.runtime.sendMessage ?
                          'runtime' : 'extension';
 
-chrome[runtimeOrExtension].sendMessage({referrer:document.referrer, url:URL}, function() {});
+chrome[runtimeOrExtension].sendMessage({referrer:REF, url:URL}, function() {});
 
 
