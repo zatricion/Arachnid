c959a4ee9acc7559f959e4dfec7c4a8945b4485d
zatricion <zatricion@gmail.com>
2013-07-26 12:21:59 -0400

    fixed pathmark choice to only show pathmarks

--- choose_mark.js
+++ choose_mark.js
@@ -1,10 +1,10 @@
 var main = function () {
-
+  var intRegex = /^\d+$/;
   var menu = document.getElementById('menu');
 
   chrome.storage.sync.get(null, function (pathmarks) {
     for (name in pathmarks) {
-      if (name !== "favicons") {
+      if (name !== "favicons" && name != "index" && !intRegex.test(name)) {
         var a = document.createElement('a');
         var linkText = document.createTextNode(name);
         a.appendChild(linkText);
