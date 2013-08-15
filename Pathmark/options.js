c72a6c532d0716d523d90c1f13e60a1c20a2be81
zatricion <zatricion@gmail.com>
2013-08-15 17:07:01 -0400

    add option to opt-out of data sending

--- Pathmark/options.js
+++ Pathmark/options.js
@@ -1,30 +1,55 @@
 function loadOptions() {
-  var select = document.getElementById("time");
+  var recents = document.getElementById("time");
   var save = document.getElementById("save");
   var restore = document.getElementById("restore");
-  
+  var sender = document.getElementById("yes");
+
   save.onclick = saveOptions;
-  restore.onclick = restoreDefault;
+  restore.onclick = restoreDefaults;
 
   var longAgo = localStorage["longAgo"];
 
   if (longAgo == undefined) {
-    restoreDefault();
+    restoreDefaultLongAgo();
   } else {
-    select.value = longAgo;
+    recents.value = longAgo;
   }
+  
+  var send = localStorage["sendToSpider"];
+
+  if (send == undefined) {
+    restoreDefaultSend();
+  } else {
+    sender.checked = JSON.parse(send);
+
+  }
+
 }
 
-function saveOptions() {
-  var select = document.getElementById("time");
-  localStorage["longAgo"] = select.value;
+function saveOptions () {
+  var recents = document.getElementById("time");
+  var sender = document.getElementById("yes");
+  localStorage["longAgo"] = recents.value;
+  localStorage["sendToSpider"] = sender.checked;
+  
   chrome.extension.getBackgroundPage().refreshOptions();
 }
 
-function restoreDefault() {
-  var select = document.getElementById("time");
-  select.value = "10";
+function restoreDefaultLongAgo () {
+  var recents = document.getElementById("time");
+  recents.value = "10";
   localStorage["longAgo"] = "10";
+}
+
+function restoreDefaultSend () {
+  var sender = document.getElementById("yes");
+  sender.checked = true;
+  localStorage["sendToSpider"] = true;
+}
+
+function restoreDefaults () {
+  restoreDefaultSend();
+  restoreDefaultLongAgo();
 }
 
 document.addEventListener("DOMContentLoaded", loadOptions, false);
