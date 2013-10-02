9005cb1834913bf716ae535465099a1a2b42c3e3
zatricion <zatricion@gmail.com>
2013-10-01 22:36:22 -0700

    renewed repo

---
+++ options.js
@@ -0,0 +1,57 @@
+function loadOptions() {
+  var recents = document.getElementById("time");
+  var save = document.getElementById("save");
+  var restore = document.getElementById("restore");
+  var sender = document.getElementById("yes");
+
+  save.onclick = saveOptions;
+  restore.onclick = restoreDefaults;
+
+  var longAgo = localStorage["longAgo"];
+
+  if (longAgo == undefined) {
+    restoreDefaultLongAgo();
+  } else {
+    recents.value = longAgo;
+  }
+  
+  var send = localStorage["sendToSpider"];
+
+  if (send == undefined) {
+    restoreDefaultSend();
+  } else {
+    sender.checked = JSON.parse(send);
+
+  }
+
+}
+
+function saveOptions () {
+  var recents = document.getElementById("time");
+  var sender = document.getElementById("yes");
+  localStorage["longAgo"] = recents.value;
+  localStorage["sendToSpider"] = sender.checked;
+  
+  chrome.extension.getBackgroundPage().refreshOptions();
+}
+
+function restoreDefaultLongAgo () {
+  var recents = document.getElementById("time");
+  recents.value = "10";
+  localStorage["longAgo"] = "10";
+}
+
+function restoreDefaultSend () {
+  var sender = document.getElementById("yes");
+  sender.checked = true;
+  localStorage["sendToSpider"] = "true";
+}
+
+function restoreDefaults () {
+  restoreDefaultSend();
+  restoreDefaultLongAgo();
+
+  chrome.extension.getBackgroundPage().refreshOptions();
+}
+
+document.addEventListener("DOMContentLoaded", loadOptions, false);
